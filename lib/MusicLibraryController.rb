43075c75542b6cad9268aeeba428e9a76b0e3be9
Cody Rickman <cody@creativemindstudios.com>
2019-07-05 19:56:59 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -14,15 +14,9 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    command = gets
+    worker(gets)
+  end
+  def worker()
+
   end
 end
-expect($stdout).to receive(:puts).with("Welcome to your music library!")
-expect($stdout).to receive(:puts).with("To list all of your songs, enter 'list songs'.")
-expect($stdout).to receive(:puts).with("To list all of the artists in your library, enter 'list artists'.")
-expect($stdout).to receive(:puts).with("To list all of the genres in your library, enter 'list genres'.")
-expect($stdout).to receive(:puts).with("To list all of the songs by a particular artist, enter 'list artist'.")
-expect($stdout).to receive(:puts).with("To list all of the songs of a particular genre, enter 'list genre'.")
-expect($stdout).to receive(:puts).with("To play a song, enter 'play song'.")
-expect($stdout).to receive(:puts).with("To quit, type 'exit'.")
-expect($stdout).to receive(:puts).with("What would you like to do?")
