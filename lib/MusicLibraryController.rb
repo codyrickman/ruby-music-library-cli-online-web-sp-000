223241ee0aaa752a17b0a893b5eb8918d06e8138
Cody Rickman <cody@creativemindstudios.com>
2019-07-05 20:02:49 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -13,8 +13,8 @@ class MusicLibraryController
     puts "To list all of the songs of a particular genre, enter 'list genre'."
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
-    puts "What would you like to do?"
     while true do
+      puts "What would you like to do?"
       command = gets
       if (command === "exit")
         break
