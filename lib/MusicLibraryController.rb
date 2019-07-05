79fb65e64af908f12be6fa195db791b0c949d647
Cody Rickman <cody@creativemindstudios.com>
2019-07-05 19:58:38 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -14,7 +14,6 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    @controller.worker(gets)
   end
   def worker(command)
     puts command
