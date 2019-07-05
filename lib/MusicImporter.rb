89bf697d8c238eb27e523e59f552c2608b9abc6b
Cody Rickman <cody@creativemindstudios.com>
2019-07-05 19:24:14 +0000

    Automatically backed up by Learn

---
+++ lib/MusicImporter.rb
@@ -0,0 +1,16 @@
+class MusicImporter
+  attr_accessor :path
+  def initialize(path)
+    @path = path
+  end
+  def files
+    files_tmp = Dir.entries(@path)
+    @files = []
+    files_tmp.each do |file|
+      if (File.basename(file) != ".." && File.basename(file) != ".")
+        @files << File.basename(file)
+      end
+    end
+    @files
+  end
+end
