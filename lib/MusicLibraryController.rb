52511eca9fd74614c363a9cd8bd859bdc85c82f1
Cody Rickman <cody@creativemindstudios.com>
2019-07-05 20:20:17 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -24,8 +24,8 @@ class MusicLibraryController
         raw_songs = @importer.files
         raw_songs.sort! { |a, b|  a<=>b}
         raw_songs.each { |song|  song.gsub!(".mp3", "")}
-        raw_songs.each do |song| 
-          songs << "#{count}. #{song}" 
+        raw_songs.each do |song|
+          songs << "#{count}. #{song}"
           count++
         end
         puts songs
